1001cd35d36932341b629f5ec17b1098df714155
xyzrlee <cnrickylee@gmail.com>
2020-02-28 18:51:21 +0800

    test

---
+++ deploy.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -e
+
+REPO_URL="https://github.com/shadowsocks/shadowsocks-libev.git"
+REPO_NAME="shadowsocks-libev"
+REPO_VERSION_FILE_DIR="."
+REPO_VERSION_FILE="Changes"
+DOCKERHUB_BUILD_BRANCH="master"
+
+[[ "${TRAVIS_BRANCH}" == "${DOCKERHUB_BUILD_BRANCH}" ]] || echo "branch [${TRAVIS_BRANCH}] not [${DOCKERHUB_BUILD_BRANCH}]" && exit 0
+
+REPO_ROOT="${HOME}/repo"
+REPO_VERSION_FILE_PATH="${REPO_ROOT}/${REPO_NAME}/${REPO_VERSION_FILE_DIR}/${REPO_VERSION_FILE}"
+
+VERSION_ROOT="${HOME}/cache/version"
+VERSION_DIR="${VERSION_ROOT}/${REPO_NAME}"
+VERSION_FILE_PATH="${VERSION_DIR}/${REPO_VERSION_FILE}"
+
+echo "REPO_ROOT=${REPO_ROOT}"
+echo "VERSION_DIR=${VERSION_DIR}"
+
+mkdir -p ${REPO_ROOT}
+mkdir -p ${VERSION_DIR}
+
+[[ -f ${VERSION_FILE_PATH}.new ]] || touch ${VERSION_FILE_PATH}.new
+rm -f ${VERSION_FILE_PATH}
+mv ${VERSION_FILE_PATH}.new ${VERSION_FILE_PATH}
+
+rm -rf ${REPO_ROOT}/${REPO_NAME}
+git clone --depth=10 ${REPO_URL} ${REPO_ROOT}/${REPO_NAME}
+cp ${REPO_VERSION_FILE_PATH} ${VERSION_FILE_PATH}.new
+
+rm -rf ${REPO_ROOT}
+
+set +e
+
+diff ${VERSION_FILE_PATH}.new ${VERSION_FILE_PATH} >/dev/null
+[[ $? == 0 ]] && echo "same" && exit 0
+
+set -e
+
+curl --request POST \
+    --header "Content-Type: application/json" \
+    --data '{"source_type": "Branch", "source_name": "${DOCKERHUB_BUILD_BRANCH}"}' \
+    ${DOCKERHUB_BUILD_TRIGGER}
