3ae5a982636098db4d2cb756ff82609fa26d4e7a
Ricky Li <cnrickylee@gmail.com>
2021-02-21 11:37:58 +0800

    Test actions (#6)

--- deploy.sh
+++ deploy.sh
@@ -2,51 +2,21 @@
 
 set -e
 
-REPO_URL="https://github.com/shadowsocks/shadowsocks-libev.git"
-REPO_NAME="shadowsocks-libev"
-REPO_VERSION_FILE_DIR="."
-REPO_VERSION_FILE="Changes"
+CACHE_DIR="${HOME}/cache"
+FILE_DIR="shadowsocks-libev"
+FILE_NAME="Changes"
 
-REPO_ROOT="${HOME}/repo"
-REPO_VERSION_FILE_PATH="${REPO_ROOT}/${REPO_NAME}/${REPO_VERSION_FILE_DIR}/${REPO_VERSION_FILE}"
+ls -alh ${CACHE_DIR}
+ls -alh ${FILE_DIR}
 
-VERSION_ROOT="${HOME}/cache/version"
-VERSION_DIR="${VERSION_ROOT}/${REPO_NAME}"
-VERSION_FILE_PATH="${VERSION_DIR}/${REPO_VERSION_FILE}"
+if [ -f "${CACHE_DIR}/${FILE_NAME}" ]
+then
+    echo "file exists"
+    exit 0
+fi
 
-echo "REPO_ROOT=${REPO_ROOT}"
-echo "VERSION_DIR=${VERSION_DIR}"
-
-mkdir -p ${REPO_ROOT}
-mkdir -p ${VERSION_DIR}
-
-[[ -f ${VERSION_FILE_PATH}.new ]] || touch ${VERSION_FILE_PATH}.new
-rm -f ${VERSION_FILE_PATH}
-mv ${VERSION_FILE_PATH}.new ${VERSION_FILE_PATH}
-
-rm -rf ${REPO_ROOT}/${REPO_NAME}
-git clone --depth=10 ${REPO_URL} ${REPO_ROOT}/${REPO_NAME}
-cp ${REPO_VERSION_FILE_PATH} ${VERSION_FILE_PATH}.new
-
-rm -rf ${REPO_ROOT}
-
-echo "new version file:"
-echo "------------------------------------"
-head -n 10 ${VERSION_FILE_PATH}.new
-echo "------------------------------------"
-echo
-echo "old version file:"
-echo "------------------------------------"
-head -n 10 ${VERSION_FILE_PATH}
-echo "------------------------------------"
-echo
-
-set +e
-
-diff ${VERSION_FILE_PATH}.new ${VERSION_FILE_PATH} >/dev/null
-[[ $? == 0 ]] && echo "same version" && exit 0
-
-set -e
+mkdir -p ${CACHE_DIR}
+cp ${FILE_DIR}/${FILE_NAME} ${CACHE_DIR}
 
 curl --request POST \
     --header "Content-Type: application/json" \
